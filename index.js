e6c9cd9951adf0e04f5862d322edc1771f8777f9
PragmaWolf <pragmawolf@gmail.com>
2022-05-20 19:39:23 +0300

    Added typedefs and little refactoring

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Function for typification and generate parameters for argument by default values
  * @param {String} defaultValue Argument default value
- * @returns {{type: String, default: Undefined, required: Boolean}|{type: String, required: Boolean, properties: Undefined}} Object with argument parameters
+ * @returns {ArgumentParameters} Object with argument parameters
  */
 const typification = defaultValue => {
     const result = {
@@ -37,7 +37,7 @@ const typification = defaultValue => {
             result.type = 'object';
             const modifiers = /[^/]*$/.exec(defaultValue);
             const def = defaultValue.replace(new RegExp(`^/|/${modifiers}$`, 'gim'), '');
-            result.default = new RegExp(def, modifiers);
+            result.default = new RegExp(def, String(modifiers));
         }
         if (defaultValue === 'Infinity') {
             result.type = (typeof Infinity);
@@ -61,14 +61,14 @@ const typification = defaultValue => {
 /**
  * Prepare array with arguments parameters by stringify function declaration.
  * @param {Function} func Function declaration.
- * @returns {{type: String, destructured: Boolean, value: Any, properties: Any, required: Boolean}[]} Array of objects with arguments parameters.
+ * @returns {FunctionArgument[]} Array of objects with arguments parameters.
  */
 module.exports = func => {
     if (typeof func !== 'function') {
         return [];
     }
     const funcStr = func.toString()
-        .replace(/\r|\n/gim, '')
+        .replace(/[\r\n]/gim, '')
         .replace(/\s{2,}/gim, ' ');
 
     const pattern = new RegExp('^((\\w*)(?=\\s*=>))|^[^(]*\\(([^)]+)\\)', 'gim');
@@ -137,3 +137,29 @@ module.exports = func => {
 
     return args;
 };
+
+/**
+ * @typedef {Object} ArgumentParameters Object with first parsed data of function argument
+ * @property {String} type Type of function argument
+ * @property {*} [default=undefined] Default value of function argument
+ * @property {Boolean} required Is the function argument required
+ * @property {Array|Undefined|DestructuredFunctionArgument[]} [properties] Destructured properties list from function argument
+ */
+
+/**
+ * @typedef {Object} FunctionArgument Object with data of function argument
+ * @property {String} name Name of function argument
+ * @property {String} [type='any'] Type of function argument
+ * @property {*} [value=undefined] Default value of function argument
+ * @property {Boolean} required If the function argument required
+ * @property {Boolean} destructured If the function argument is destructured
+ * @property {Array.<DestructuredFunctionArgument>} properties Properties list of destructured function arguments
+ */
+/**
+ * @typedef {Object} DestructuredFunctionArgument Object with data of destructured function argument
+ * @property {String} name Name of function argument
+ * @property {String} [type='any'] Type of function argument
+ * @property {*} [value=undefined] Default value of function argument
+ * @property {Boolean} required If the function argument required
+ * @property {Boolean} [destructured=false] If the function argument is destructured
+ */
